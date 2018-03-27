72d38811d480d9c492d9ce8ffd6b445991344518
dmattek <macdobry@gmail.com>
2018-03-27 23:49:40 +0200

    Bug fixes

--- analConn.R
+++ analConn.R
@@ -4,23 +4,38 @@
 
 require(data.table)
 
+args <- commandArgs(TRUE)
+
 # params
 params = list()
 
-# core of the file name with CP output
-# tracking output is in a directory build from this core
-params$s.file.core = 'objNuclei_'
+## User-defined input
 
-# name of the folder with CP output
-params$s.dir.data = '.'
-params$s.dir.tracks = paste0('trackXY_', params$s.file.core)
+# Path to CP output
+# This directory is the root for all following directories
+# E.g. myexp1/cp.out1/output/out_0001
+params$s.dir.data = args[1]
+params$s.dir.data = '/Volumes/imaging.data/Paolo/MCF10A_TimeLapse/2018-03-26_MCF10Amutants_H2B-miRFP_ERKKTR-Turq_FoxO-NeonGreen_40xAir_T5min_Stim15min-1ngmlEGF_24h-starving+CO2/cp.out2/output/out_0001'
 
-# file with output from CP segmentation (1-line header file)
-params$s.file.data.cp = paste0(params$s.file.core,'.csv')
+# Core of the file name with CP output, e.g. objNuclei_1line
+params$s.file.core = args[2]
+params$s.file.core = 'objNuclei_1line'
 
-# file with track connectivities
-params$s.file.data.con = paste0(params$s.file.core, '_WellA1_S00_conn.csv')
-params$s.file.data.seq = paste0(params$s.file.core, '_WellA1_S00_seq.csv')
+# Directory, relative to root, to LAP output, e.g. tracksXY_objNuclei_1line
+params$s.dir.tracks = args[3]
+params$s.dir.tracks = 'trackXY_objNuclei_1line'
+
+# minimum track length
+params$tracklen = as.integer(args[4])
+params$tracklen = 10
+
+## Create paths based on user input
+
+# file with output from CP segmentation (must be a 1-line header file)
+params$s.file.data.cp = paste0(params$s.file.core, '.csv')
+
+# suffix for the output file with clean tracks
+params$s.file.track.suff = '_clean_tracks.csv'
 
 
 # column names
@@ -32,33 +47,27 @@ params$s.objnum.cp = 'objNuclei_ObjectNumber'
 params$s.track = 'track_id'
 params$s.trackuni = paste0(params$s.track, "_uni")
 
-# minimum track length
-params$tracklen = 10
 
 # switch variable for data output
 # set FALSE if you don't want processed data to be written to files
 params$b.data.out = TRUE
 
-# get directory from the command line
-args <- commandArgs(TRUE)
-
-if (is.na(args[1]))
-  params$s.currdirr = '.' else
-    params$s.currdirr = paste0(getwd(), '/', args[1])
-
 
 cat('Setting current directory to:\n')
-cat(params$s.currdirr, '\n\n')
-setwd(params$s.currdirr)
+cat(params$s.dir.data, '\n\n')
+setwd(params$s.dir.data)
 
-# setting path manually
-#params$s.currdirr = '~/Projects/Olivier/Paolo/2018-01-14_2018-01-18_merged/cp.out'
-#params$s.currdirr = '~/Projects/Olivier/Paolo/2017-12-20_MCF10Amutants_H2B-miRFP_ERKKTR-mTurquiose_20xAir_T30s_noStim/cp3.out/'
+# file with track connectivities
+s.track.files = list.files(file.path(params$s.dir.data, params$s.dir.tracks), "*.csv")
+
+params$s.file.data.con = s.track.files[s.track.files %like% 'conn']
+params$s.file.data.seq = s.track.files[s.track.files %like% 'seq']
+
 
 # load data
-dt.cp = fread(file.path(params$s.currdirr, params$s.dir.data, params$s.file.data.cp))
-dt.con = fread(file.path(params$s.currdirr, params$s.dir.data, params$s.dir.tracks, params$s.file.data.con))
-dt.seq = fread(file.path(params$s.currdirr, params$s.dir.data, params$s.dir.tracks, params$s.file.data.seq))
+dt.cp = fread(file.path(params$s.dir.data, params$s.file.data.cp))
+dt.con = fread(file.path(params$s.dir.data, params$s.dir.tracks, params$s.file.data.con))
+dt.seq = fread(file.path(params$s.dir.data, params$s.dir.tracks, params$s.file.data.seq))
 
 # Matlab output is entirely numeric
 # Metadata_Well column contains numbers that enumnerate consecutive wells from the original table
@@ -103,5 +112,6 @@ setkeyv(dt.concp.sub, c(params$s.well, params$s.site, params$s.track))
 # ONLY CELLS with tracks longer than the threshold params$tracklen
 v.meascol = names(dt.concp.sub)[names(dt.concp.sub) %like% 'obj']
 write.csv(x = dt.concp.sub[, (c(params$s.well, params$s.site, params$s.time, params$s.track, params$s.pm.condall, v.meascol)), with = FALSE], 
-          file = file.path(params$s.currdirr, params$s.dir.data, gsub('.csv', '_clean_tracks.csv', params$s.file.data.cp)), 
-          row.names = F)
+          file = file.path(params$s.dir.data, paste0(params$s.file.core, params$s.file.track.suff)), 
+          row.names = F, quote = F)
+
--- cleanCPout.R
+++ cleanCPout.R
@@ -1,16 +1,19 @@
 # cleans 2-line header output from CP
 # converts to a 1-line header
 # removes selected columns
+#
+# Example call from command-line
+# Rscript cleanCPout.R objNuclei.csv objNuclei_1line.csv
 
 args <- commandArgs(TRUE)
 
 # input file
 s.f.in = args[1]
-  #'/mnt/imaging.data/Paolo/MCF10A_TimeLapse/2018-03-26_MCF10Amutants_H2B-miRFP_ERKKTR-Turq_FoxO-NeonGreen_40xAir_T5min_Stim15min-1ngmlEGF_24h-starving+CO2/cp.out2/output/out_0001/objNuclei.csv'
 
 # output file
 s.f.out = args[2]
-  #'/mnt/imaging.data/Paolo/MCF10A_TimeLapse/2018-03-26_MCF10Amutants_H2B-miRFP_ERKKTR-Turq_FoxO-NeonGreen_40xAir_T5min_Stim15min-1ngmlEGF_24h-starving+CO2/cp.out2/output/out_0001/objNuclei_1line.csv'
+
+# list of column names to remove
 s.cols.rm = c('Image_Metadata_C',
               'Image_Metadata_Channel',
               'Image_Metadata_ChannelName',
@@ -27,6 +30,10 @@ s.cols.rm = c('Image_Metadata_C',
               'Image_Metadata_Series')
   
 require(tca)
+# read a CSV with a 2-line header; 
+# remove repeated columns; 
+# remove columns according to the list in s.cols.rm
 dt=freadCSV2lineHeader(s.f.in, s.cols.rm)
 
-write.csv(x = dt, file = s.f.out, row.names = F)
+# save file; no row numbers, no quotes around strings
+write.csv(x = dt, file = s.f.out, row.names = F, quote = F)
