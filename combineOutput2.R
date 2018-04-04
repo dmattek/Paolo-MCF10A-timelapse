c086c989013db15eafe2b25c831d79c14ae501e8
Access from microscope PC <mic01@izblisbon.campus.unibe.ch>
2018-04-05 00:05:17 +0200

    Added: merging regular csv's from sub-directories

--- combineOutput2.R
+++ combineOutput2.R
@@ -14,38 +14,30 @@ params = list()
 # Path to CP output
 # This directory is the root for a directory that contains sub-directories
 # E.g. myexp1/cp.out1/output
-params$s.dir.data = args[1]
-
-# Core of the file name with CP output, e.g. objNuclei_1line_clean_tracks
-params$s.file.core = args[2]
-
-# extension of files with CP output
-params$s.file.pattern = '.csv'
-
 if (is.na(args[1]))
-  params$s.currdirr = '.' else
-    params$s.currdirr = paste0(getwd(), '/', args[1])
+  params$s.dir.data = '.' else
+    params$s.dir.data = args[1]
 
-
+# Core of the file name with CP output, e.g. objNuclei_1line_clean_tracks.csv
 if (is.na(args[2]))
-  params$s.file.core = 'output'
-  
+  params$s.file.data = 'output.csv' else
+    params$s.file.data = args[2]
+
 # Create directory for merged output in the current working directory
-params$s.dir.output = paste0(params$s.dir.data, '.mer')
-ifelse(!dir.exists(file.path(params$s.currdirr, params$s.dir.output)), 
-       dir.create(file.path(params$s.currdirr, params$s.dir.output)), 
+params$s.dir.out = paste0(params$s.dir.data, '.mer')
+ifelse(!dir.exists(file.path(params$s.dir.out)), 
+       dir.create(file.path(params$s.dir.out)), 
        FALSE)
 
 
 # store locations of all csv files in the output folder
-s.files = list.files(path = file.path(params$s.currdirr, params$s.dir.data), 
-                     pattern = paste0(params$s.file.core, params$s.file.pattern), 
+s.files = list.files(path = file.path(params$s.dir.data), 
+                     pattern = params$s.file.data, 
                      recursive = TRUE, 
                      full.names = TRUE)
 
 dt.all = do.call(rbind, lapply(s.files, fread))
                                
 # write merged dataset
-if (params$b.data.out) {
-  write.csv(dt.all, file = file.path(params$s.currdirr, params$s.dir.output, paste0(params$s.file.core, params$s.file.pattern)), row.names = F) 
-}
+write.csv(dt.all, file = file.path(params$s.dir.out, params$s.file.data), row.names = F) 
+
---
+++ combout.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR=/opt/local/misc-improc/Paolo-MCF10A-timelapse
+
+runrscript.sh $DIR/combineOutput2.R $@
